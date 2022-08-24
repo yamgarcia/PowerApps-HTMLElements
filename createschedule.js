0877d92ff8b5a13babbb5989a5055f27a31d488d
yamgarcia <yamgarcia.ca@gmail.com>
2022-08-24 12:34:11 -0700

    Working version on both functions

--- createschedule.js
+++ createschedule.js
@@ -1,12 +1,37 @@
-var Schedule = window.Contact || {};
+var Schedule = window.Schedule || {};
 
 (function () {
-  this.onScheduleCreate = function () {
-    alert("Hello world! This function is on CreateContact.js");
-    debugger;
+  this.onWorkdayCreate = function () {
     var Xrm = window.parent.Xrm;
-    // formContext.ui.setFormNotification("You clicked on custom button", "INFO", "myButtonId");
+    var accountId = Xrm.Page.data.entity
+      .getId()
+      .replace("{", "")
+      .replace("}", "");
 
+    //Sunday = 1, Monday = 2, Tuesday = 3, Wednesday = 4, Thursday = 5, Friday = 6, Saturday = 7
+
+    const openhour = document.querySelector("#openhour");
+    const closehour = document.querySelector("#closehour");
+
+    var data = {
+      crf93_weekday: document.getElementById("weekday").value,
+      crf93_openinghour: openhour.value,
+      crf93_closinghour: closehour.value,
+      "trr_Business@odata.bind": "/accounts(" + accountId + ")",
+    };
+
+    Xrm.WebApi.createRecord("crf93_schedule", data).then(
+      function success(result) {
+        console.log("Schedule created with contactid: " + result.id);
+      },
+      function (error) {
+        console.log(error.message);
+      }
+    );
+  };
+
+  this.onScheduleCreate = function () {
+    var Xrm = window.parent.Xrm;
     var accountId = Xrm.Page.data.entity
       .getId()
       .replace("{", "")
@@ -20,10 +45,10 @@ var Schedule = window.Contact || {};
         crf93_weekday: i,
         crf93_openinghour: "09:00 am",
         crf93_closinghour: "05:00 pm",
-        "parentcustomerid_account@odata.bind": "/accounts(" + accountId + ")",
+        "trr_Business@odata.bind": "/accounts(" + accountId + ")",
       };
-
-      Xrm.WebApi.createRecord("contact", data).then(
+      debugger;
+      Xrm.WebApi.createRecord("crf93_schedule", data).then(
         function success(result) {
           console.log(
             `Workday number ${i}. Schedule created with contactid:  ${result.id}`
@@ -31,43 +56,8 @@ var Schedule = window.Contact || {};
         },
         function (error) {
           console.log(error.message);
-          // handle error conditions
         }
       );
     }
   };
-
-  this.onWorkdayCreate = function () {
-    alert("Hello world! This function is on CreateContact.js");
-    debugger;
-    var Xrm = window.parent.Xrm;
-    // formContext.ui.setFormNotification("You clicked on custom button", "INFO", "myButtonId");
-
-    var accountId = Xrm.Page.data.entity
-      .getId()
-      .replace("{", "")
-      .replace("}", "");
-
-    //Sunday = 1, Monday = 2, Tuesday = 3, Wednesday = 4, Thursday = 5, Friday = 6, Saturday = 7
-
-    const openhour = document.querySelector(".openhour");
-    const closehour = document.querySelector(".closehour");
-
-    var data = {
-      crf93_weekday: document.getElementById("weekday").value,
-      crf93_openinghour: openhour.value,
-      crf93_closinghour: closehour.value,
-      "parentcustomerid_account@odata.bind": "/accounts(" + accountId + ")",
-    };
-
-    Xrm.WebApi.createRecord("contact", data).then(
-      function success(result) {
-        console.log("Contact created with contactid: " + result.id);
-      },
-      function (error) {
-        console.log(error.message);
-        // handle error conditions
-      }
-    );
-  };
-}.call(Contact));
+}.call(Schedule));
